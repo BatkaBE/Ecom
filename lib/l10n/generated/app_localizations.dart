4c17afd7628ef5a9d6eda9918941e6e14051b9d7
SmoovyxD <batkabata42@gmail.com>
2025-05-30 21:12:53 +0800

    eniigee oruulchiidaa

---
+++ lib/l10n/generated/app_localizations.dart
@@ -0,0 +1,117 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/intl.dart' as intl;
+
+import 'app_localizations_en.dart';
+import 'app_localizations_mn.dart';
+
+
+abstract class AppLocalizations {
+  AppLocalizations(String locale) : localeName = intl.Intl.canonicalizedLocale(locale.toString());
+
+  final String localeName;
+
+  static AppLocalizations? of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations);
+  }
+
+  static const LocalizationsDelegate<AppLocalizations> delegate = _AppLocalizationsDelegate();
+
+  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates = <LocalizationsDelegate<dynamic>>[
+    delegate,
+    GlobalMaterialLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+  ];
+
+  /// A list of this localizations delegate's supported locales.
+  static const List<Locale> supportedLocales = <Locale>[
+    Locale('en'),
+    Locale('mn')
+  ];
+  String get shop;
+
+
+  String get basket;
+
+  String get basketState;
+
+
+  String get favorites;
+
+  String get profile;
+
+  String get myProfile;
+
+  String get myOrders;
+
+
+  String get shippingAddresses;
+
+
+  String get paymentMethods;
+
+
+  String get promotionCodes;
+
+
+  String get myReviews;
+
+  String get settings;
+
+
+  String priceLabel(Object price);
+
+
+  String categoryLabel(Object category);
+
+
+  String ratingLabel(Object count, Object rating);
+
+
+  String get total;
+
+
+  String get paymentSuccess;
+
+
+  String get buy;
+
+  get product => null;
+
+}
+
+class _AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
+  const _AppLocalizationsDelegate();
+
+  @override
+  Future<AppLocalizations> load(Locale locale) {
+    return SynchronousFuture<AppLocalizations>(lookupAppLocalizations(locale));
+  }
+
+  @override
+  bool isSupported(Locale locale) => <String>['en', 'mn'].contains(locale.languageCode);
+
+  @override
+  bool shouldReload(_AppLocalizationsDelegate old) => false;
+}
+
+AppLocalizations lookupAppLocalizations(Locale locale) {
+
+
+  // Lookup logic when only language code is specified.
+  switch (locale.languageCode) {
+    case 'en': return AppLocalizationsEn();
+    case 'mn': return AppLocalizationsMn();
+  }
+
+  throw FlutterError(
+    'AppLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
+    'an issue with the localizations generation tool. Please file an issue '
+    'on GitHub with a reproducible sample app and the gen-l10n configuration '
+    'that was used.'
+  );
+}
